ef8596732892e74c8af4004c6b5b8cd7d7cbbc0a
RobertKiliszewski <robert.kiliszewski@hotmail.com>
2017-10-04 12:46:38 +0100

    Problem 8
    Problem 8

---
+++ 08-ListSort.py
@@ -0,0 +1,15 @@
+#Python problem 8 
+#This program merges and sorts 2 lists together 
+#Created by: Robert Kiliszewski	
+#04/10/2017
+
+list1 = [1, 4, 6]
+list2 = [2, 3, 5]
+
+listMerge = list1 + list2
+
+print()
+#Merged List
+print("Merged List", listMerge)
+#Merged and Sorted List
+print("Sorted List", sorted(listMerge))
